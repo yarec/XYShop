d2fdbf91f1e75c82fab976a6b33fd4441469c37b
yarec <yarec@163.com>
2018-04-16 05:07:02 +0000

    pc demo

---
+++ app/Http/Controllers/Common/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Common;
+
+use App\Http\Controllers\Common\Ueditor;
+use App\Http\Controllers\Controller;
+use App\Models\Common\Attr;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public $theme = 'pc';
+
+}
